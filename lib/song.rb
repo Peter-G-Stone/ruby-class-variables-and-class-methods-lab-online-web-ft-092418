3a7a57d4ec3b223cc9a7a5fc1256141d43c366bd
Peter Stone <peter.stone.online@gmail.com>
2018-10-01 15:19:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
-  
+  @@genres = []
   
   def initialize(name, artist, genre)
     @@count += 1 
+    @@genres << genre 
   end 
   
   def count 
@@ -12,6 +13,6 @@ class Song
   end 
   
   def genres 
-    
-    
+    @@genres 
+  end 
 end 
